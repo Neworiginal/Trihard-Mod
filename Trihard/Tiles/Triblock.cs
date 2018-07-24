146cbed19b195d7b52bb946e24db791d2f1acbe0
Neworiginal <kyleizzy123@outlook.com>
2018-07-24 21:20:28 -0230

    v0.1.5

---
+++ Trihard/Tiles/Triblock.cs
@@ -0,0 +1,37 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trihard.Tiles
+{
+    public class Triblock : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Tridirt");
+            Tooltip.SetDefault("I shitted and farted");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("Triplacable"); //put your CustomBlock Tile name
+        }
+        public override void AddRecipes()  //How to craft this item
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Wood, 10);   //you need 10 Wood
+            recipe.AddTile(TileID.WorkBenches);   //at work bench
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Trihard/Weapon/TerroistRPG.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Trihard.Weapon   //this is where is this file
+{
+    public class TerroistRPG : ModItem  //this is the class name
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Terroist RPG");
+            Tooltip.SetDefault(";)");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 650;      //this is the damage of the item
+            item.ranged = true;   //Whether the damage this item deals is considered ranged
+            item.width = 20;
+            item.height = 20;
+            item.shootSpeed = 0.1f;
+            item.useTime = 4;        //How long it takes to use the item, in frames
+            item.useAnimation = 29;    //How long it takes the animation to play for the item
+            item.useStyle = 5;      //The animation that is played when the item is used
+            item.knockBack = 5;  //this is the weapon knockback
+            item.crit = 4;   //the weapon critical strike
+            item.UseSound = SoundID.Item11;   //The type of sound to play when the item is used
+            item.value = Item.buyPrice(30, 0, 0, 0); // How much the item is worth, in copper coins, when you sell it to a merchant. It costs 1/5th of this to buy it back from them. An easy way to remember the value is platinum, gold, silver, copper or PPGGSSCC (so this item price is 10gold)
+            item.rare = 11;//The color to draw the item's name in
+            item.shoot = ProjectileID.RocketI;  //this is the type of ammo the weapon shoots.  
+            item.shootSpeed = 16f; //this is the speed of the projectile that is created          
+            item.useAmmo = 771; //this is the type of ammo the weapon use
+            item.autoReuse = true;  //true = autofire, add false if u don't want to autofire
+            item.noMelee = true;  //Whether this item is allowed to deal damage with its sprite. Typically set to true for a ranged weapon, which would rely on its projectiles to deal damage, instead.
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 3 + Main.rand.Next(2); // 4 or 5 shots
+            for (int i = 0; i < numberProjectiles; i++)
+            {
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(20)); // 30 degree spread.
+                                                                                                                // If you want to randomize the speed to stagger the projectiles
+                                                                                                                // float scale = 1f - (Main.rand.NextFloat() * .3f);
+                                                                                                                // perturbedSpeed = perturbedSpeed * scale; 
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+            }
+            return false; // return false because we don't want tmodloader to shoot projectile
+        }
+
+        public override void AddRecipes()
+        {                                        //this is how to craft this item
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.LunarBar, 9999);   //this is how to add an ingredient from Terraria,  so for craftin this item u need 1 Dirt Block
+            recipe.AddTile(TileID.Anvils);   //this is where to craft the item ,    Anvils = all anvils , MythrilAnvil = Mythril Anvil and Orichalcum Anvil, Furnaces = all furnaces , DemonAltar = Demon Altar and Crimson Altar , TinkerersWorkbench = Tinkerer's Workbench
+            recipe.SetResult(this);  //there you can set the output and the quantity of the item crafting
+            recipe.AddRecipe();
+        }
+        public override Vector2? HoldoutOffset()
+        {                                     //this make the player hold the weapon like a cannon/rocket launcher
+            return Vector2.Zero;
+        }
+    }
+}
